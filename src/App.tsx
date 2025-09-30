03901fc03504c661031b9f7f423b4046c733ab79
mikez <zeldabakhos6@gmail.com>
2025-09-30 11:01:06 +0200

    Finalized CardComponent feature

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,20 @@ const App = () => {
   	const [gameCards, setGameCards] = useState<TCardList>(cards)
 	console.log(gameCards);
 
+	const handleCardClick = (clickedCard: TCard) => {
+		// Flip the card
+		setGameCards((prev) =>
+			prev.map((card) =>
+				card.id === clickedCard.id ? { ...card, flipped: !card.flipped} : card
+			)
+		)
+	}
 	return (
 		<div className="main_section">
 		<h1>Memory Game</h1>
 		<div className="card_container">
 			{gameCards.map((card: TCard) => {
-        	return <CardComp card={card} clickProp={() => {}} key={card.id} />
+        	return <CardComp card={card} clickProp={handleCardClick} key={card.id} />
 			})}
 		</div>
 		</div>
--- src/components/CardComp.tsx
+++ src/components/CardComp.tsx
@@ -2,21 +2,24 @@ import type { TCard } from "../types/card.types"
 import styles from "./CardComp.module.css"
 
 export type TCardProps = {
-  clickProp: () => void
-  card: TCard
+	clickProp: (card: TCard) => void
+	card: TCard
 }
 
 const CardComp = ({ clickProp, card }: TCardProps) => {
-  const handleClick = () => {
-    console.log("clicked", card)
-    clickProp()
-  }
+	const handleClick = () => {
+		console.log("clicked", card)
+		clickProp(card)
+	}
 
-  return (
-    <article onClick={handleClick} className={styles.card}>
-      CardComp
-    </article>
-  )
+	return (
+		<article
+			onClick={handleClick}
+			className={`${styles.card} ${card.flipped ? styles.animate__rotate : ""}`}
+		>
+			CardComp
+		</article>
+	)
 }
 
 export default CardComp
