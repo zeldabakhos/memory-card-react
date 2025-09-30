030189115a656c135d92d4e61e02ae9e27af29e4
mikez <zeldabakhos6@gmail.com>
2025-09-30 11:23:23 +0200

    Game logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,6 @@ import type { TCardList, TCard } from "./types/card.types"
 
 const App = () => {
 
-	const handleCardClick = (clickedCard: TCard) => {
-		// Flip the card
-		setGameCards((prev) =>
-			prev.map((card) =>
-				card.id === clickedCard.id ? { ...card, flipped: !card.flipped} : card
-			)
-		)
-	}
-
 	// Create pairs of cards
 	const createGameCards = (): TCardList => {
 		const pairs = cards.flatMap((card) => [
@@ -30,6 +21,56 @@ const App = () => {
 		shuffleCards(createGameCards()) 
 	)	
 	console.log(gameCards);
+
+	const [flippedCards, setFlippedCards] = useState<TCard["name"][]>([])
+
+	const handleCardClick = (clickedCard: TCard) => {
+		// Check if the card is already matched
+		if (clickedCard.matched) return
+
+		// Check if we have 2 cards flipped already
+		if (flippedCards.length === 2) return
+
+		// Flip the card
+		setGameCards((prev) =>
+			prev.map((card) =>
+			card.id === clickedCard.id ? { ...card, flipped: !card.flipped } : card
+			)
+		)
+
+		setFlippedCards((prev) => [...prev, clickedCard["name"]])
+		}
+
+	useEffect(() => {
+		if (flippedCards.length === 2) {
+			// Check if the flipped cards match
+			const [firstCard, secondCard] = flippedCards
+			if (firstCard === secondCard) {
+				console.log("match")
+				setFlippedCards([]) // empty the flipped cards array
+
+				// set the matched cards to true
+				setGameCards((prev) =>
+					prev.map((card) =>
+					card.name === firstCard ? { ...card, matched: true } : card
+					)
+				)
+			} else {
+				// Flip the cards back
+				setTimeout(() => {
+					setGameCards((prev) =>
+					prev.map((card) =>
+						// find the cards to flip back to avoid flipping all of them
+						flippedCards.some((fc) => fc === card.name)
+						? { ...card, flipped: false }
+						: card
+					)
+					)
+					setFlippedCards([]) // empty the flipped cards array
+				}, 1000)
+			}	
+		}
+	}, [flippedCards])
 
 	return (
 		<div className="main_section">
